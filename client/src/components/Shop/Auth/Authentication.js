8d92ebf10c2d7a3ebb0b8b97ab8a3dd3066df67b
Khanh Nguyen <nqkhanh357@gmail.com>
2022-09-02 15:39:15 +0700

    start project - day 1

--- client/src/components/Shop/Auth/Authentication.js
+++
@@ -1,8 +0,0 @@
-export const isAuth = () => (localStorage.getItem('jwt') ? JSON.parse(localStorage.getItem('jwt')) : false);
-
-export const isAdmin = () => (localStorage.getItem('jwt') ? JSON.parse(localStorage.getItem('jwt')).user.role === 1 : false);
-
-export const logout = () => {
-	localStorage.clear();
-	window.location.href = '/';
-};
--- client/src/components/Shop/index.js
+++
@@ -1,5 +0,0 @@
-import { layoutReducer, layoutState } from './Layout/LayoutContext';
-import { LayoutContext } from './Layout/Layout';
-import Home from './Home/Home';
-
-export { LayoutContext, layoutReducer, layoutState, Home };
